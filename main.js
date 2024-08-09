2017164b461ea23f6a3f7ce8d7c63b372cab8678
Jithgarm <jonathansmg919@gmail.com>
2024-08-09 07:27:17 -0500

    Subiendo ejercicio de fibonacci

---
+++ main.js
@@ -0,0 +1,42 @@
+const entrada = document.getElementById("entrada")
+const btn = document.getElementById("btn")
+const texto = document.getElementById("texto")
+
+
+function fibo(n){
+    let salida =[0,1]
+    if(n==1){
+        salida = [0]
+    }else if( n==2){
+        salida = [0,1]
+    }else{
+        for( let i =3; i<=n;i++){
+            dato = salida[i-2] + salida[i-3]
+            salida.push(dato)
+            console.log(salida)
+        }
+    }
+    return salida
+}
+
+function numberToText(n,textoUsar){
+    text = ""
+    for(let i=1;i<=n;i++){
+        text= text + `${textoUsar}`
+    }
+    return text
+}
+
+entrada.addEventListener("input",(evento)=>{
+    numero = evento.target.value
+    console.log(numero)
+})
+
+btn.addEventListener("click",()=>{
+    let secuencia = fibo(numero)
+    secuencia.forEach((dato)=>{
+        let hijo = document.createElement("h3")
+        hijo.innerHTML = numberToText(dato,"|")
+        texto.appendChild(hijo)
+    })
+})
